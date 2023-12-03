a469ad6880a0139405c45525252aa675d9a15834
MaheshRautrao <101188065+MaheshRautrao@users.noreply.github.com>
2023-12-04 00:50:05 +0530

    input todo added

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,5 +1,23 @@
 function App() {
-  return <>mahes</>;
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center h-screen text-white bg-green-900 ">
+        <div>
+          <input
+            type="text"
+            className="p-1 text-black rounded-sm outline-none"
+            placeholder="Write your todo"
+          />
+          <button className="px-5 py-2 text-white bg-black rounded-lg">
+            Add Todo
+          </button>
+        </div>
+        <div>
+          <div>mahesh</div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
